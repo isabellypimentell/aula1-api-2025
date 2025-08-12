f456f27b8a7b167821b80013e366935b8267efe3
isabellypimentell <126909881+isabellypimentell@users.noreply.github.com>
2025-08-12 19:45:55 +0000

    Aula 12/08/2025

---
+++ src/main/java/application/repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package application.repository;
+
+import org.springframework.data.jpa.repository.Jparepository;
+
+import application.model.Tarefa;
+
+public class TarefaRepository extends JpaRepository<Tarefa, Long> {
+    
+}
